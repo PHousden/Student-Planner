84e6cdd854f901f8c2f34af6da5d44ab421bcf58
Philip Housden <94585775+PHousden@users.noreply.github.com>
2022-03-12 17:40:06 +0700

    Put in information to the pie chart

--- src/JS/main.js
+++ src/JS/main.js
@@ -7,6 +7,21 @@ function buttonClicked() {
   const sSubject = document.getElementsByClassName("second_subjects");
   const tSubject = document.getElementsByClassName("third_subjects");
 }
+DataEC = {
+    datasets = [{
+        backgroundColor: backgroundCategory,
+        data: costGroupedByCategory.amount,
+        borderColor: 'rgba(0,0,0,0.1)', 
+        borderWidth: '1'
+    }]
+            ,
+            labels: costGroupedByCategory.category
+}
+
+var optionsEC = {
+    title: {disply: true, text: "The Planing to the study", fontSize: '24', fontFamily: "'Roboto', sans-serif"},
+    legend: {disply: true, position: 'right', aling: 'center'}
+}
 
 var Pie = document.getElementById('pieChart').getContext('2d');
 PieChart = new Chart(Pie, { type: 'pie', data: DataEC, options: optionsEC});
