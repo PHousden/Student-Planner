efd59111c00beb9fd78ba729205f6e1f3cd7027c
Philip Housden <94585775+PHousden@users.noreply.github.com>
2022-03-12 20:47:19 +0700

    fixing Pie Chart

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,9 +1,19 @@
+let timeSpend = document.querySelector(".time_spend").value;
+let breakTime = document.querySelector(".break_time").value;
+let fSubjectV = document.querySelector(".first_subjects");
+let sSubjectV = document.querySelector(".second_subjects");
+let tSubjectV = document.querySelector(".third_subjects");
 
-let timeSpend = document.querySelector(".time_spend");
-let breakTime = document.querySelector(".break_time");
-let fSubject = document.querySelector(".first_subjects");
-let sSubject = document.querySelector(".second_subjects");
-let tSubject = document.querySelector(".third_subjects");
+
+  
+  let fSubject = (50 / 100) * (timeSpend + breakTime);
+  let sSubject = (25 / 100) * (timeSpend + breakTime);
+  let tSubject = (25 / 100) * (timeSpend + breakTime);
+
+  
+
+
+const backgroundcolor = ["red", "blue", "purple", "green", "yellow"];
 
 let myChart = document.getElementById("myChart").getContext("2d");
 
@@ -14,39 +24,26 @@ document.querySelector("button").addEventListener("click", () => {
       labels: [
         "Time spend",
         "Break Time",
-        "First Subject",
-        "Second Subject",
-        "Third Subject",
+        fSubjectV.value,
+        sSubjectV.value,
+        tSubjectV.value,
       ],
       datasets: [
         {
           label: "Plan",
-          data: [
-            timeSpend.value,
-            breakTime.value,
-            fSubject.value,
-            sSubject.value,
-            tSubject.value,
-          ],
+          data: [timeSpend.value, breakTime.value, fSubject.value, sSubject.value, tSubject.value],
         },
       ],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-    ],
-    borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-    ],
-    borderWidth: 1
+      backgroundColor: backgroundcolor,
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
     },
     options: {},
   });
