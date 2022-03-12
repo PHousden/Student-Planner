904132f5b85529e40bac914f695efd8a3b0da285
Philip Housden <94585775+PHousden@users.noreply.github.com>
2022-03-12 21:43:07 +0700

    adding more input

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,50 +1,42 @@
-let timeSpend = document.querySelector(".time_spend").value;
-let breakTime = document.querySelector(".break_time").value;
-let fSubjectV = document.querySelector(".first_subjects");
-let sSubjectV = document.querySelector(".second_subjects");
-let tSubjectV = document.querySelector(".third_subjects");
+let timeSpend = document.querySelector("#time_spend");
+let breakTime = document.querySelector("#break_time");
+let fSubject = document.querySelector("#first_subjects");
+let sSubject = document.querySelector("#second_subjects");
+let tSubject = document.querySelector("#third_subjects");
+let mostCon = document.querySelector("#first_percentage");
+let secCon = document.querySelector("#second_percentage");
+let thirdCon = document.queryCommandValue("#third_percentage");
 
-
-  
-  let fSubject = (50 / 100) * (timeSpend + breakTime);
-  let sSubject = (25 / 100) * (timeSpend + breakTime);
-  let tSubject = (25 / 100) * (timeSpend + breakTime);
-
-  
-
-
-const backgroundcolor = ["red", "blue", "purple", "green", "yellow"];
+let fSubjectV = (mostCon/100) * (timeSpend + breakTime); 
+let sSubjectV = (secCon/100)* (timeSpend + breakTime);
+let tSubjectV = (thirdCon/100)* (timeSpend + breakTime);
 
 let myChart = document.getElementById("myChart").getContext("2d");
 
 document.querySelector("button").addEventListener("click", () => {
-  let pieChart = new Chart(myChart, {
-    type: "pie",
-    data: {
-      labels: [
-        "Time spend",
-        "Break Time",
-        fSubjectV.value,
-        sSubjectV.value,
-        tSubjectV.value,
-      ],
-      datasets: [
-        {
-          label: "Plan",
-          data: [timeSpend.value, breakTime.value, fSubject.value, sSubject.value, tSubject.value],
+    let pieChart = new Chart(myChart, {
+        type: "pie",
+        data: {
+            labels: [
+                "Time spend",
+                "Break Time",
+                fSubject.value,
+                sSubject.value,
+                tSubject.value,
+            ],
+            datasets: [
+                {
+                    label: "Plan",
+                    data: [
+                        timeSpend.value,
+                        breakTime.value,
+                        mostCon.value,
+                        secCon.value,
+                        thirdCon.value,
+                    ],
+                },
+            ],
         },
-      ],
-      backgroundColor: backgroundcolor,
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 1,
-    },
-    options: {},
-  });
+        options: {},
+    });
 });
