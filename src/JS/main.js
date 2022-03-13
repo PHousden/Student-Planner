2c372f59244e96e909f29d72ed143a95ca493ddd
Philip Housden <94585775+PHousden@users.noreply.github.com>
2022-03-13 09:04:53 +0700

    adding about page and custome it more

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,42 +1,52 @@
-let timeSpend = document.querySelector("#time_spend");
-let breakTime = document.querySelector("#break_time");
-let fSubject = document.querySelector("#first_subjects");
-let sSubject = document.querySelector("#second_subjects");
-let tSubject = document.querySelector("#third_subjects");
-let mostCon = document.querySelector("#first_percentage");
-let secCon = document.querySelector("#second_percentage");
-let thirdCon = document.queryCommandValue("#third_percentage");
+let btnFinished = document.querySelector('button');
+console.log(btnFinished);
+let myTable = document.querySelector('#table-result');
+var timeSpend = document.querySelector('#time_spend').value;
+var breakTime = document.querySelector('#break_time').value;
+var sj1 = document.querySelector('#first_subjects').value;
+var sj2 = document.querySelector('#second_subjects').value;
+var sj3 = document.querySelector('#third_subjects').value;
 
-let fSubjectV = (mostCon/100) * (timeSpend + breakTime); 
-let sSubjectV = (secCon/100)* (timeSpend + breakTime);
-let tSubjectV = (thirdCon/100)* (timeSpend + breakTime);
 
-let myChart = document.getElementById("myChart").getContext("2d");
+btnFinished.onclick = function(){
 
-document.querySelector("button").addEventListener("click", () => {
-    let pieChart = new Chart(myChart, {
-        type: "pie",
-        data: {
-            labels: [
-                "Time spend",
-                "Break Time",
-                fSubject.value,
-                sSubject.value,
-                tSubject.value,
-            ],
-            datasets: [
-                {
-                    label: "Plan",
-                    data: [
-                        timeSpend.value,
-                        breakTime.value,
-                        mostCon.value,
-                        secCon.value,
-                        thirdCon.value,
-                    ],
-                },
-            ],
-        },
-        options: {},
+    let subjects = [
+        {subjects: sj1.value, study_time: (0.5*(timeSpend+breakTime)), break: breakTime.value },
+        {subjects: sj2.value, study_time: (0.25*(timeSpend+breakTime)), break: breakTime.value  },
+        {subjects: sj3.value, study_time: (0.25*(timeSpend+breakTime)), break: breakTime .value },
+    ];
+    console.log(subjects);
+}
+
+    let header = ['Subjects', 'Study Time', 'Break'];
+
+    btnFinished.addEventListener('click', () => {
+        let table = document.createElement('table');
+        let headerRow = document.createElement('tr');
+
+        header.forEach(headerText => {
+            let header = document.createElement('th');
+            let textNode = document.createTextNode(headerText);
+            header.appendChild(textNode);
+            headerRow.appendChild(header);
+        });
+
+        table.appendChild(headerRow);
+
+        subjects.forEach(sub => {
+            let row = document.createElement('tr');
+
+            Object.values(sub).forEach(text => {
+                let cell = document.createElement('td');
+                let textNode = document.createTextNode(text);
+                cell.appendChild(textNode);
+                row.appendChild(cell);
+            })
+
+            table.appendChild(row);
+        });
+
+        myTable.appendChild(table);
+
     });
-});
+
