b59cdec61763dd3f366b6bf6e8424e187c0f5303
Philip Housden <94585775+PHousden@users.noreply.github.com>
2022-03-15 20:18:00 +0700

    adding pie chart successfully

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,52 +1,41 @@
-let btnFinished = document.querySelector('button');
-console.log(btnFinished);
-let myTable = document.querySelector('#table-result');
-var timeSpend = document.querySelector('#time_spend').value;
-var breakTime = document.querySelector('#break_time').value;
-var sj1 = document.querySelector('#first_subjects').value;
-var sj2 = document.querySelector('#second_subjects').value;
-var sj3 = document.querySelector('#third_subjects').value;
-
-
-btnFinished.onclick = function(){
-
-    let subjects = [
-        {subjects: sj1.value, study_time: (0.5*(timeSpend+breakTime)), break: breakTime.value },
-        {subjects: sj2.value, study_time: (0.25*(timeSpend+breakTime)), break: breakTime.value  },
-        {subjects: sj3.value, study_time: (0.25*(timeSpend+breakTime)), break: breakTime .value },
-    ];
-    console.log(subjects);
-}
-
-    let header = ['Subjects', 'Study Time', 'Break'];
-
-    btnFinished.addEventListener('click', () => {
-        let table = document.createElement('table');
-        let headerRow = document.createElement('tr');
-
-        header.forEach(headerText => {
-            let header = document.createElement('th');
-            let textNode = document.createTextNode(headerText);
-            header.appendChild(textNode);
-            headerRow.appendChild(header);
-        });
-
-        table.appendChild(headerRow);
-
-        subjects.forEach(sub => {
-            let row = document.createElement('tr');
-
-            Object.values(sub).forEach(text => {
-                let cell = document.createElement('td');
-                let textNode = document.createTextNode(text);
-                cell.appendChild(textNode);
-                row.appendChild(cell);
-            })
-
-            table.appendChild(row);
-        });
-
-        myTable.appendChild(table);
-
+document.querySelector("button").addEventListener("click", () => {
+    let timeSpend = document.querySelector("#time_spend").value;
+    let breakTime = document.querySelector("#break_time").value;
+    let fSubject = document.querySelector("#first_subjects").value;
+    let sSubject = document.querySelector("#second_subjects").value;
+    let tSubject = document.querySelector("#third_subjects").value;
+    let mostCon = document.querySelector("#first_percentage").value;
+    let secCon = document.querySelector("#second_percentage").value;
+    let thirdCon = document.querySelector("#third_percentage").value;
+  
+    let fSubjectV = (mostCon / 100) * (timeSpend + breakTime);
+    let sSubjectV = (secCon / 100) * (timeSpend + breakTime);
+    let tSubjectV = (thirdCon / 100) * (timeSpend + breakTime);
+  
+    const canvasEle = document.createElement("canvas");
+    canvasEle.id = "myChart";
+    let myChart = canvasEle.getContext("2d");
+    const firstEle = document.getElementsByClassName("container")[0].children[0];
+    if (firstEle) {
+      document
+        .getElementsByClassName("container")[0]
+        .replaceChild(canvasEle, firstEle);
+    } else {
+      document.getElementsByClassName("container")[0].appendChilde(canvasEle);
+    }
+  
+    let pieChart = new Chart(myChart, {
+      type: "pie",
+      data: {
+        labels: ["Time spend", "Break Time", fSubject, sSubject, tSubject],
+        datasets: [
+          {
+            label: "Plan",
+            data: [timeSpend, breakTime, mostCon, secCon, thirdCon],
+            backgroundColor: ["black", "red", "orange", "green", "blue"]
+          }
+        ]
+      },
+      options: {}
     });
-
+  });
